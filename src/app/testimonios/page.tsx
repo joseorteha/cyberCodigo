b6e3619bce99c8290b6c4072730edbea179d61ab
joseorteha <joseortegahac@gmail.com>
2025-06-21 13:51:42 -0600

    debug(deploy): Temporarily disable TestimonialForm

--- src/app/testimonios/page.tsx
+++ src/app/testimonios/page.tsx
@@ -1,5 +1,11 @@
 import TestimonialForm from '../../components/TestimonialForm';
 
 export default function TestimoniosPage() {
-  return <TestimonialForm />;
+  // return <TestimonialForm />;
+  return (
+    <div style={{ padding: '5rem', textAlign: 'center' }}>
+      <h1>Página de Testimonios en Mantenimiento</h1>
+      <p>Estamos realizando ajustes. ¡Volveremos pronto!</p>
+    </div>
+  );
 } 
